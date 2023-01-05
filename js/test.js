9ffcb815af498b28c52c628fe14110911d3628b0
Hayashi Jiro <89639320+hayashijiro@users.noreply.github.com>
2023-01-06 01:17:57 +0700

    Update test.js
    Signed-off-by: Hayashi Jiro <89639320+hayashijiro@users.noreply.github.com>

--- js/test.js
+++ js/test.js
@@ -1,13 +1,10 @@
-var browsername=navigator.appName;
-if( browsername == "" )
-{
-   window.location="";
-}
-else if ( browsername =="")
-{
-   window.location="";
-}
-else
-{
-   window.location="";
-}
+document.querySelector('.anh').addEventListener('click', function () {
+    window.open("https://hayashijiro.github.io/dichamthanh/",
+            "name_your_window",
+            "location=1,status=1,scrollbars=1,resizable=no,menubar=no,toolbar=no");
+})
+document.querySelector('.mic').addEventListener('click', function () {
+    window.open("https://hayashijiro.github.io/dichanh/",
+            "name_your_window",
+            "location=1,status=1,scrollbars=1,resizable=no,menubar=no,toolbar=no");
+})
